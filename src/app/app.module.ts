688adec2b2899881b7b96eabf581f15e38bcbab1
José Miguel Conde Martín <josemy1403@gmail.com>
2024-05-12 14:38:31 +0200

    Fin de Sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { heroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    heroesModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:
+    [AppComponent],
 })
 export class AppModule { }
