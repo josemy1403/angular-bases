b6bcbef6a6bcd29ecd6256b71f3d91284d3233cc
José Miguel Conde Martín <josemy1403@gmail.com>
2024-05-14 20:29:44 +0200

    corrección componente counter

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -6,19 +6,19 @@ import { Component, OnInit } from '@angular/core';
 
   <button (click)="increaseBy(1)">+1</button>
   <button (click)="resetCounter()">Reset</button>
-  <button (click)="decreaseBy(-1)">-1</button>`,
+  <button (click)="decreaseBy(1)">-1</button>`,
 })
 
 export class CounterComponent implements OnInit {
   constructor() { }
 
 
-  public counter:number = 10;
+  public counter: number = 10;
 
-  increaseBy( value: number ): void {
+  increaseBy(value: number): void {
     this.counter += value;
   }
-  decreaseBy( value: number ): void {
+  decreaseBy(value: number): void {
     this.counter -= value;
   }
   resetCounter(): void {
