f97c5bbab56b2d03186e6faf5c4039293eba8e6a
José Miguel Conde Martín <josemy1403@gmail.com>
2024-05-19 17:22:44 +0200

    seccion 5 video 69

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -13,5 +13,17 @@ export class ListComponent {
     name: 'Trunks',
     power: 10
   }]
+
+
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+
+  onDeleteCharacter(index: number): void {
+    // TODO: Emitir el id del personaje
+
+    this.onDelete.emit(index);
+    console.log('Evento emitido index del list component', index)
+  }
 
 }
